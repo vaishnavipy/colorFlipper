bd4f3590de9815d225271ad41e476f98b7806d8a
Vaishnavi ravichandran <vaishnaviravichandran@Vaishnavis-MacBook-Air.local>
2020-09-29 12:34:50 -0500

    namechange

--- hex.js
+++ hex.js
@@ -3,18 +3,15 @@ window.onload = function(){
 
 
   
-   let color_array;let randomcolor;
+    let randomcolor;
 
-   
 document.getElementById("btn").onclick = function displayColor(){
 
+
+    randomcolor = (Math.random()*0xfffff * 1000000).toString(16);
+    randomcolor = "#"+randomcolor.slice(0,6);
     
-    let random_num = Math.floor(Math.random()*4);
-
-    color_array = ["red","green","#F15025","rgba(133,122,200)"];
-
-    randomcolor= color_array[random_num];
-  
+         
 
     document.getElementById("hex_color").innerHTML = randomcolor;
     document.getElementById("body").style.backgroundColor = randomcolor;
